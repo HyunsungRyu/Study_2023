369684c22f3b4165bebd8d7ade042764f66d83e7
HyunsungRyu <seal0130@gmail.com>
2023-09-07 21:04:30 +0900

    20230907

--- git.py
+++ git.py
@@ -1,1 +1,14 @@
-print("Final_test")
+import tensorflow as tf
+
+
+x_data = [1, 2, 3, 4, 5]
+y_data = [1, 2, 3, 4, 5]
+
+W = tf.Variable(2.9)
+b = tf.Variable(0.5)
+
+# hypothesis = W * x + b
+hypothesis = W * x_data + b
+
+# cost(W, b)
+cost = tf.reduce_mean(tf.square(hypothesis - y_data))
