4d7826b84affd03731bc99dc42beeb3d4ff44d1b
HyunsungRyu <seal0130@gmail.com>
2023-11-06 17:19:56 +0900

    20231106

--- ASpy.py
+++ ASpy.py
@@ -1,3 +1,4 @@
+"""
 # 셀레니움_기본설정
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -66,3 +67,36 @@ time.sleep(2)
 # 로그인 버튼
 login_btn = driver.find_element(By.CSS_SELECTOR, "#log\.login")
 login_btn.click()
+""" """
+import requests
+from bs4 import BeautifulSoup
+
+# naver 서버에 대화를 시도
+response = requests.get("https://finance.naver.com/")
+
+# naver에서 html 줌
+html = response.text
+
+# html 번역선성님으로 수프를 만듦
+soup = BeautifulSoup(html, "html.parser")
+
+# id 값이 NM_set_home_btn인 놈 한개를 찾아냄
+Word = soup.select_one("#menu > ul > li.m1.first.on > a > span.tx")
+
+# 텍스트 요소만 출력
+print(Word)
+"""
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get(
+    "https://search.naver.com/search.naver?where=news&sm=tab_jum&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90"
+)
+html = response.text
+soup = BeautifulSoup(html, "html.parser")
+links = soup.select(".news_tit")  # 결과는 리스트
+
+for link in links:
+    title = link.text  # 태그 안에 텍스트요소를 가져온다
+    url = link.attrs["href"]  # href의 속성값을 가져온다
+    print(title, url)
